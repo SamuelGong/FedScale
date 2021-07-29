888c70c22ee16a93ea426b096cedea83378c14be
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-07-29 23:42:09 +0800

    ns

--- core/resource_manager.py
+++ core/resource_manager.py
@@ -7,12 +7,17 @@ class ResourceManager(object):
 
         self.client_run_queue = []
         self.client_run_queue_idx = 0
+        self.client_all_test_queue = []
+        self.client_all_test_queue_idx = 0
 
     
     def register_tasks(self, clientsToRun):
         self.client_run_queue = clientsToRun
         self.client_run_queue_idx = 0
 
+    def register_all_test_tasks(self, clientsToRun):
+        self.client_all_queue = clientsToRun
+        self.client_all_test_queue_idx = 0
 
     def get_next_task(self):
         if self.client_run_queue_idx < len(self.client_run_queue):
@@ -22,3 +27,10 @@ class ResourceManager(object):
 
         return None
 
+    def get_next_all_test_task(self):
+        if self.client_all_test_queue_idx < len(self.client_all_test_queue):
+            clientId = self.client_all_test_queue[self.client_all_test_queue_idx]
+            self.client_all_test_queue_idx += 1
+            return clientId
+
+        return None
