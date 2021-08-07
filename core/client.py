d335a8b4de7d43b58c7b2f85e851d661636a7699
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-08-07 20:55:13 +0800

    ns

--- core/client.py
+++ core/client.py
@@ -152,13 +152,13 @@ class Client(object):
                         target_c = target.clone()
                         output_c = client_model(data_c)
                         _ = criterion(output_c, target_c)
-                        print(type(data), type(data_c))
-                        print(type(target), type(target_c))
+                        logging.info(f"{type(data)} {type(data_c)}")
+                        logging.info(f"{type(target)} {type(target_c)}")
 
                         cnt = 0
                         for param_c, param in zip(client_model.parameters(), model.parameters()):
                             if cnt == 0:
-                                print(type(param_c), type(param_c.data), type(param_c.grad))
+                                logging.info(f"{type(param_c)} {type(param_c.data)} {type(param_c.grad)}")
                             cnt += 1
                             eff_grad = param_c.grad.clone() + lam * (param_c.data.detach().clone() -
                                                                      param.data.detach().clone())
