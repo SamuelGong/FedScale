fe05c9b43861bf7848bd75b850fe379cf999445f
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-08-07 22:41:14 +0800

    ns

--- core/evals/cleanup_model.sh
+++ core/evals/cleanup_model.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+CurrentDir=.
+LogDir=~/models
+
 cd .
 for file in *;do
   if [[ "$file" == *"logging"* ]];then
-    pair=$(grep -o 'time_stamp=[^ ,]\+' $file)
-    folder=$(echo $pair | sed "s/.*time_stamp='\(.*\)'.*/\1/g")
-    echo $folder
+    p=$(grep -o 'time_stamp=[^ ,]\+' $file)
+    folder=$(echo $p | sed "s/.*time_stamp='\(.*\)'.*/\1/g")
+
+    q=$(grep -o 'model=[^ ,]\+' $file)
+    model=$(echo $q | sed "s/.*model='\(.*\)'.*/\1/g")
+
+
   fi
 done
