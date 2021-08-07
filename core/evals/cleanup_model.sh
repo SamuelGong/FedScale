e988976aabe3685fc98fa83d3698cb66ddc0512c
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-08-07 23:21:32 +0800

    ns

--- core/evals/cleanup_model.sh
+++ core/evals/cleanup_model.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-CurrentDir=.
+CurrentDir=$(pwd)
 LogDir=~/models
+useful=()
 
-cd .
+cd $CurrentDir
 for file in *;do
   if [[ "$file" == *"logging"* ]];then
     p=$(grep -o 'time_stamp=[^ ,]\+' $file)
@@ -12,6 +13,22 @@ for file in *;do
     q=$(grep -o 'model=[^ ,]\+' $file)
     model=$(echo $q | sed "s/.*model='\(.*\)'.*/\1/g")
 
-
+    useful[${#useful[@]}]=$folder
   fi
 done
+
+for folder in ${useful[@]}; do
+  echo $folder
+done
+
+cd $LogDir/$model
+
+for file in *;do
+  if [[ "$folder" == *"logging"* ]];then
+    if [[ ! " ${useful[@]} " =~ " $folder " ]]; then
+      echo $folder
+    fi
+  fi
+done
+
+cd $CurrentDir
