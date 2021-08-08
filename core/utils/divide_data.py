09811b75899f39d6679ab8c29843730836d734c0
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-08-08 13:13:29 +0800

    ns

--- core/utils/divide_data.py
+++ core/utils/divide_data.py
@@ -121,11 +121,11 @@ def select_dataset(rank, partition, batch_size, isTest=False, collate_fn=None):
     partition = partition.use(rank - 1, isTest)
     dropLast = False if isTest else True
     num_loaders = min(int(len(partition)/args.batch_size/2), args.num_loaders)
-    # if num_loaders == 0:
-    #     time_out = 0
-    # else:
-    #     time_out = 60
-    time_out = 0
+    if num_loaders == 0:
+        time_out = 0
+    else:
+        time_out = 60
+    # time_out = 0
 
     if collate_fn is not None:
         return DataLoader(partition, batch_size=batch_size, shuffle=True, pin_memory=True, timeout=time_out, num_workers=num_loaders, drop_last=dropLast, collate_fn=collate_fn)
