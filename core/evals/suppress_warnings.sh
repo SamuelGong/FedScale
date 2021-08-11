93707a37a6e6ed434691be88669db602ca6f4b12
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-08-11 11:48:35 +0800

    ms

--- core/evals/suppress_warnings.sh
+++ core/evals/suppress_warnings.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-original=$(pwd)
-cd $1
 
-for file in *;do
-  if [[ "$file" == *"logging"* ]];then
-    grep -v "Caffe2" $file > tmp && mv tmp $file
-  fi
-done
+if [ -d $1 ]; then
+  cd $1
+  original=$(pwd)
 
-cd $original
+  for file in *;do
+    if [[ "$file" == *"logging"* ]];then
+      grep -v "Caffe2" $file > suppress_tmp && mv suppress_tmp $file
+    fi
+  done
+
+  cd $original
+elif [ -f $1 ]; then
+  grep -v "Caffe2" $1 > suppress_tmp && mv suppress_tmp $1
+fi
