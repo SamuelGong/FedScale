2d968ba848b6b58209b8559bb3dfef020d21aefe
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-08-14 19:31:36 +0800

    ms

--- core/evals/copy_log.sh
+++ core/evals/copy_log.sh
@@ -1,16 +1,47 @@
 CurrentDir=$(pwd)
 
+LogDir=~/models
+DestDir=CurrentDir/log
+time_stamps=()
+models=()
+job_names=()
+
 for file in *;do
   if [[ "$file" == *"info"* ]];then
-#    p=$(grep -o 'time_stamp=[^ ,]\+' $file)
-#    folder=$(echo $p | sed "s/.*time_stamp='\(.*\)'.*/\1/g")
-#
-#    q=$(grep -o 'model=[^ ,]\+' $file)
-#    model=$(echo $q | sed "s/.*model='\(.*\)'.*/\1/g")
-#    if [[ ! " ${models[@]} " =~ " $model " ]]; then
-#      models[${#models[@]}]=$model
-#    fi
-#
-#    useful[${#useful[@]}]=$folder
+    p=$(grep -o 'time_stamp=[^ ,]\+' $file)
+    time_stamp=$(echo $p | sed "s/.*time_stamp=\(.*\).*/\1/g")
+
+    q=$(grep -o 'model=[^ ,]\+' $file)
+    model=$(echo $q | sed "s/.*model=\(.*\).*/\1/g")
+
+    r=$(grep -o 'job_name=[^ ,]\+' $file)
+    job_name=$(echo $q | sed "s/.*job_name=\(.*\).*/\1/g")
+
+    time_stamps[${#time_stamps[@]}]=$time_stamp
+    models[${#models[@]}]=$model
+    job_names[${#job_names[@]}]=$job_name
   fi
 done
+
+arraylength=${#job_names[@]}
+for (( i=0; i<${arraylength}; i++ ));
+do
+  job_name="${job_names[$i]}"
+  dest=${DestDir}/${job_name}
+
+  cd $LogDir/${models[$i]}/${time_stamps[$i]}
+  mkdir -p $dest/aggregator
+  cd aggregator
+  cp log $dest/aggregator
+
+  mkdir -p $dest/executor
+  cd ../executor
+
+  for file in *;do
+    if [[ "$file" == *"log"* ]];then
+      cp $file $dest/executor
+    fi
+  done
+done
+
+cd $CurrentDir
