2199fa2ac6cfe2367dc341828cc24fe2c70ab62d
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-08-15 17:45:22 +0800

    ms

--- core/client.py
+++ core/client.py
@@ -105,8 +105,7 @@ class Client(object):
             delta = 1e-3
             beta = 0.01
         elif conf.personalized == "ditto":
-            # lam = 3e-1
-            lam = 1.0
+            lam = 3e-1
             loop_num = 2
         else:
             loop_num = 1
