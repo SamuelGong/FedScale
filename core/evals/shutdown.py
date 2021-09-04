0755e648ad76b100462315dd443f1c645544e68e
JIANG Zhifeng <zjiangaj@rgcpu7.cse.ust.hk>
2021-09-04 09:50:27 +0800

    ns

--- core/evals/shutdown.py
+++ core/evals/shutdown.py
@@ -5,8 +5,8 @@ import argparse
 job_name = sys.argv[1]
 
 if job_name == 'all':
-    pass
     #os.system("ps -ef | grep python | grep FedScale > fedscale_running_temp")
+    pass
 else:
     os.system("ps -ef | grep python | grep job_name={} > fedscale_running_temp".format(job_name))
 [os.system("kill -9 "+str(l.split()[1]) + " 1>/dev/null 2>&1") for l in open("fedscale_running_temp").readlines()]
