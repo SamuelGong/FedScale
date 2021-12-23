0e399b698170f6631e4f8d2e8e1c7c1235d38350
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-12-24 00:19:43 +0800

    ns

--- dataset/prepare_stackoverflow.py
+++ dataset/prepare_stackoverflow.py
@@ -2,11 +2,14 @@ import os
 
 # relative path to this file
 root_dir = "data/reddit"
-client_data_mapping_path = os.path.join(root_dir, "client_data_mapping")
+client_data_mapping_dir = os.path.join(root_dir, "client_data_mapping")
 train_data_dir = os.path.join(root_dir, "train")
+train_mapping_path = os.path.join(client_data_mapping_dir, "train.csv")
 test_data_dir = os.path.join(root_dir, "test")
+test_mapping_path = os.path.join(client_data_mapping_dir, "test.csv")
 
 # test
-print(os.path.isfile(client_data_mapping_path))
+print(os.path.isfile(train_mapping_path))
+print(os.path.isfile(test_mapping_path))
 print(os.path.isdir(train_data_dir))
 print(os.path.isdir(test_data_dir))
