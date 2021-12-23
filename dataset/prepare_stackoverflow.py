da19c85fa54fb1b84a0e7ae95aa297a47abe3d51
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-12-24 00:17:15 +0800

    ns

--- dataset/prepare_stackoverflow.py
+++ dataset/prepare_stackoverflow.py
@@ -7,6 +7,6 @@ train_data_dir = os.path.join(root_dir, "train")
 test_data_dir = os.path.join(root_dir, "test")
 
 # test
-print(os.isfile(client_data_mapping_path))
-print(os.isdir(train_data_dir))
-print(os.isdir(test_data_dir))
+print(os.path.isfile(client_data_mapping_path))
+print(os.path.isdir(train_data_dir))
+print(os.path.isdir(test_data_dir))
