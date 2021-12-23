174156123babf672b4cf08916d9f32e18f4001c4
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-12-24 00:31:29 +0800

    ns

--- dataset/prepare_stackoverflow.py
+++ dataset/prepare_stackoverflow.py
@@ -1,5 +1,18 @@
 import os
 
+# dependencies
+import sys
+import subprocess
+import pkg_resources
+
+required = {'transformers'}
+installed = {pkg.key for pkg in pkg_resources.working_set}
+missing = required - installed
+
+if missing:
+    python = sys.executable
+    subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
+
 # Albert over StackOverflow
 model = "albert-base-v2"
 from transformers import (
