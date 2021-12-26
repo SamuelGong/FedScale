3406efd9b3aadfd53b5ed5abd198158b1d43f8b2
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-12-26 10:49:28 +0800

    ns

--- dataset/prepare_stackoverflow.py
+++ dataset/prepare_stackoverflow.py
@@ -4,6 +4,7 @@ import collections
 import copy
 import numpy as np
 from multiprocessing import Pool, cpu_count
+from torch.utils.data import DataLoader, Dataset
 
 N_JOBS = 16
 N_JOBS = 1
@@ -29,7 +30,6 @@ from transformers import (
     AutoModelForMaskedLM
 )
 config = AutoConfig.from_pretrained(model_name)
-model = AutoModelForMaskedLM.from_config(config)
 tokenizer = AlbertTokenizer.from_pretrained(model_name, do_lower_case=True)
 
 # relative path to this file
@@ -83,6 +83,18 @@ def chunks_idx(l, n):
         yield si, si+(d+1 if i < r else d)
 
 
+class TextDataset(Dataset):
+    def __init__(self, inputs, labels):
+        self.inputs = inputs
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.inputs)
+
+    def __getitem__(self, index):
+        return (self.inputs[index], self.labels[index])
+
+
 print(f"[Debug] [A] Elapsed time: {time.perf_counter() - start_time}")
 
 
@@ -120,8 +132,6 @@ def feature_creation_worker(files, tokenizer, block_size, worker_idx):
 
     inputs, labels = mask_tokens(examples, tokenizer)
     print(f"Tokens masked.")
-    print(inputs)
-    print(labels)
 
     return (inputs, labels, client_mapping, sample_client)
 
@@ -138,7 +148,7 @@ files = [entry.name for entry in os.scandir(train_data_dir) if '_cached_lm_' not
 # make sure files are ordered
 files = [os.path.join(train_data_dir, x) for x in sorted(files)]
 
-files = files[:10]
+files = files[:200]
 
 print(f"[Debug] [B] Elapsed time: {time.perf_counter() - start_time}")
 
@@ -165,3 +175,45 @@ for (input, label, client_mapping, sample_client) in pool_outputs:
     user_id_base = true_sample_client[-1] + 1
 
 print(f"[Debug] [C] Elapsed time: {time.perf_counter() - start_time}")
+
+### Testing training
+model = AutoModelForMaskedLM.from_config(config)
+learning_rate = 4e-5
+batch_size = 20
+
+no_decay = ["bias", "LayerNorm.weight"]
+optimizer_grouped_parameters = [
+    {
+        "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+        "weight_decay": conf.weight_decay,
+    },
+    {
+        "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+        "weight_decay": 0.0,
+    },
+]
+
+optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=learning_rate)
+criterion = torch.nn.CrossEntropyLoss(reduction='none')
+dataset = Dataset(inputs, labels)
+client_data = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, drop_last=False)
+
+local_steps = 20
+completed_steps = 0
+
+train_start_time = time.perf_counter()
+while completed_steps < local_steps:
+    for (data, target) in client_data:
+        outputs = model(data, labels=target)
+        loss = outputs[0]
+        loss_list = [loss.item()]
+        temp_loss = sum([l ** 2 for l in loss_list]) / float(len(loss_list))
+
+        train_time = time.perf_counter() - train_start_time
+        print(f"[Step {completed_steps}] "
+              f"temp_loss: {temp_loss}, train_time: {train_time}")
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        completed_steps += 1
