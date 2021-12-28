4aea904568daa79a79a2967301eb66dcdc5207e6
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-12-28 11:19:42 +0800

    ns

--- dataset/prepare_stackoverflow.py
+++ dataset/prepare_stackoverflow.py
@@ -177,7 +177,7 @@ def prepare_data(data_dir, block_size, clip):
         sample_clients += true_sample_clients
         for user_id, true_user_id in zip(sample_clients, true_sample_clients):
             client_mapping[true_user_id] = client_mapping[user_id]
-        user_id_base = true_sample_client[-1] + 1
+        user_id_base = true_sample_clients[-1] + 1
 
     return inputs, labels, client_mapping, sample_clients
 
