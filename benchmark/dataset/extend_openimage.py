b6003314d503384f478975827d285cfba2199fda
Zhifeng JIANG <samuelgong2017@gmail.com>
2023-08-01 13:47:05 +0800

    A

--- benchmark/dataset/extend_openimage.py
+++ benchmark/dataset/extend_openimage.py
@@ -2,31 +2,33 @@ import csv
 
 num_clients = 1000
 
-csv_file = 'data/openImg/client_data_mapping/train.csv'
+mapping_path = 'data/openImg/client_data_mapping/train.csv'
 sample_id = 0
-read_first = True
-raw_clients = {}
 labels = []
-csv_reader = csv.reader(csv_file, delimiter=',')
 
-for row in csv_reader:
-    if read_first:
-        read_first = False
 
-    else:
-        # client_id,sample_path,label_name,label_id
-        client_id = row[0]
-        label = int(row[3])
+with open(mapping_path) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    read_first = True
+    raw_clients = {}
 
-        if client_id not in raw_clients:
-            if len(raw_clients.keys()) \
-                    == num_clients:
-                break
-            raw_clients[client_id] = []
+    for row in csv_reader:
+        if read_first:
+            read_first = False
+        else:
+            # client_id,sample_path,label_name,label_id
+            client_id = row[0]
+            label = int(row[3])
 
-        raw_clients[client_id].append(sample_id)
-        labels.append(label)
-        sample_id += 1
+            if client_id not in raw_clients:
+                if len(raw_clients.keys()) \
+                        == num_clients:
+                    break
+                raw_clients[client_id] = []
+
+            raw_clients[client_id].append(sample_id)
+            labels.append(label)
+            sample_id += 1
 
 empty_data_list = []
 for client_id in range(1, num_clients + 1):
